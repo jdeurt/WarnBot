61b4aa15f28517c7a512595c5be65a511aa9c19a
Sharky! <jdeurt@gmail.com>
2018-04-12 19:26:07 -0500

    Update warner.js

--- warner.js
+++ warner.js
@@ -11,15 +11,18 @@ function toSQLReadable(str = "") {
 }
 
 bot.on("message", msg => {
-    if(!msg.content.startsWith("!warn") || msg.channel.type !== "text" || !msg.mentions.users) return;
+    if(!msg.content.startsWith("!warn") || msg.channel.type !== "text" || !msg.mentions.users || !msg.content.split(" ")[1].includes("@"))) return;
     let user = msg.mentions.users.first();
+    let reason = (msg.content.split(" ").length > 2) ? (msg.content.split(" ").slice(2).join(" ")) : ("--No reason stated.--");
     sql.open("./warnings.sqlite").then(() => {
         sql.get(`SELECT * FROM main WHERE id = "${toSQLReadable(user.id)}"`).then(row => {
             if(!row) sql.run("INSERT INTO main (id, amt) VALUES (?, ?)", [toSQLReadable(user.id), 1]).then(() => {
                 msg.channel.send(`**Warned:** \`${user.tag}\``);
+                user.send(`You have been warned in \`${msg.guild.name}\` (\`${msg.channel.name}\`) \`\`\`${reason}\`\`\` `);
             });
             else sql.run(`UPDATE main SET amt = ${row.amt+1} WHERE id = "${toSQLReadable(user.id)}"`).then(() => {
                 msg.channel.send(`**Warned:** \`${user.tag}\``);
+                user.send(`You have been warned in \`${msg.guild.name}\` (\`${msg.channel.name}\`) \`\`\`${reason}\`\`\` `);
             });
         }).catch(() => {
             sql.run("CREATE TABLE IF NOT EXISTS main (id TEXT, amt INTEGER)").then(() => {
